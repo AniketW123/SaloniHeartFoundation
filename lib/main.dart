ba65468d050df2e2381ad5668e0c4d9ae4a13338
Warty <aniket.warty25@bcp.org>
2022-12-23 14:02:55 -0800

    basic bottom navigation bar functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,17 +8,40 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: Home(),
+    home: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  List<Widget> pages = <Widget> [Home(),];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: "Information",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_support),
+            label: "Contact",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart_fill),
+            label: "Donate",
+          ),
+        ],
+      ),
+      body: Center(
 
+      ),
     );
   }
 }
