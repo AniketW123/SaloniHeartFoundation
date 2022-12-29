d1134be308507a0a794d63422d4d7d92dc187ffa
Warty <aniket.warty25@bcp.org>
2022-12-29 12:23:36 -0800

    added our mission text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../util/listtile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +9,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextStyle missionStyle = TextStyle(fontSize: 15, color: Colors.white);
+  TextStyle missionStyleBold = TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-
+    return ListView(
+      children: [
+        RoundedListTile(
+          title: 'Our Mission',
+          color: Colors.blue.shade900,
+          body: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(text: '• Patient Consultation: ', style: missionStyleBold),
+                TextSpan(text: 'Provide free unbiased consultation from the world\'s Top doctors to families of children with complex heart condition.\n', style: missionStyle),
+                TextSpan(text: '• Doctor Training: ', style: missionStyleBold),
+                TextSpan(text: 'Provide free unbiased consultation from the world\'s Top doctors to families of children with complex heart condition.\n', style: missionStyle),
+                TextSpan(text: '• Patient Consultation: ', style: missionStyleBold),
+                TextSpan(text: 'Provide free unbiased consultation from the world\'s Top doctors to families of children with complex heart condition.\n', style: missionStyle),
+              ]
+            ),
+          ),
+        )
+      ],
     );
   }
 }
